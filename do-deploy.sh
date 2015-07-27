ff92c77368163f47f9f28eab9ca1ef737a9a3ba3
Witold Rugowski <rugowski@nhw.pl>
2015-07-27 12:09:26 +0200

    zmiana skryptu do deploy by obsługiwał nr wersji

--- do-deploy.sh
+++ do-deploy.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
 
+echo "Tagi z GITa:"
+echo
+git tag
+echo "---------"
+echo "Podaj numer wersji. Będzie częscią pliku"
+read ans
+for i in *.zip; do
+	name=`basename $i .zip`
+	echo "$name.zip => $name-$ans.zip"
+done
+echo "OK? (y/n)"
+read confirm
+
+if [ "$confirm" = "y" ]; then
+	for i in *.zip; do
+		name=`basename $i .zip`
+		scp $name.zip static.nettigo.pl:NTG-STATIC/$name-$ans.zip
+	done
+
+fi
+
+exit 0
+
 ./do-release.sh
 scp *.zip static.nettigo.pl:NTG-STATIC/
