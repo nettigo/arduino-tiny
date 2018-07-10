3d0ba0a64430fb9c6d5f5c36bcfddf84730d15cd
Witold Rugowski <rugowski@nhw.pl>
2018-07-10 15:31:25 +0200

    Rozbudowa narzędzi do deploy

--- local-deploy.sh
+++ local-deploy.sh
@@ -1,6 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
+# kopuje bieżące developerskie tinyBrd core do katalogu Arduino, nadpisuje pierwszy znaleziony core
+
 DEST_DIR="$HOME/.arduino15/packages/tinybrd/hardware/avr/"
 
 INSTALLED=`ls -1 $DEST_DIR |head -1`
-#rm -fR ${INSTALLED}/*
+rm -fR ${DEST_DIR}/${INSTALLED}/*
 cp -fR NettigoTinyBrd/avr/* ${DEST_DIR}$INSTALLED
+
+echo "Wgrano developerską wersję w miejsce wersji ${INSTALLED}"
